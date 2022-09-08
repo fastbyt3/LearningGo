991aecee751d81706dc8f8180b230973e8116276
Fastbyt3 <aldeerayan@gmail.com>
2022-09-08 21:37:29 +0530

    Reformated weather cli app

--- 14 - Cobra CLI/weather_app/cmd/fetch.go
+++ 14 - Cobra CLI/weather_app/cmd/fetch.go
@@ -9,47 +9,47 @@ import (
 )
 
 type Response struct {
-    Requests Request `json:"request"`
-    Locations Loc `json:"location"`
-    Currents Current `json:"current"`
+	Requests  Request `json:"request"`
+	Locations Loc     `json:"location"`
+	Currents  Current `json:"current"`
 }
 
 type Request struct {
-    Type string
-    Query string
-    Lang string
-    Unit string
+	Type  string
+	Query string
+	Lang  string
+	Unit  string
 }
 
 type Loc struct {
-    Name string
-    Country string
-    Region string
-    Lat string
-    Lon string
-    TimeZoneId string
-    LocalTime string
-    LocalTimeEpoch int
-    UTCOffset string
+	Name           string
+	Country        string
+	Region         string
+	Lat            string
+	Lon            string
+	TimeZoneId     string
+	LocalTime      string
+	LocalTimeEpoch int
+	UTCOffset      string
 }
 
 type Current struct {
-    Time string
-    Temp int `json:"temperature"`
-    WeatherCode int `json:"weather_code"`
-    WeatherIcons []string `json:"weather_icons"`
-    WeatherDescs []string `json:"weather_descriptions"`
-    WindSpeed int `json:"wind_speed"`
-    WindDeg int `json:"wind_degree"`
-    WindDir string `json:"wind_degree"`
-    Pressure int `json:"pressure"`
-    Precip int `json:"precip"`
-    Humidity int `json:"humidity"`
-    Cloudcover int `json:"cloudcover"`
-    FeelsLike int `json:"feelslike"`
-    UVIndex int `json:"uv_index"`
-    Visibiltiy int `json:"visibility"`
-    IsDay string `json:"is_day"`
+	Time         string
+	Temp         int      `json:"temperature"`
+	WeatherCode  int      `json:"weather_code"`
+	WeatherIcons []string `json:"weather_icons"`
+	WeatherDescs []string `json:"weather_descriptions"`
+	WindSpeed    int      `json:"wind_speed"`
+	WindDeg      int      `json:"wind_degree"`
+	WindDir      string   `json:"wind_degree"`
+	Pressure     int      `json:"pressure"`
+	Precip       int      `json:"precip"`
+	Humidity     int      `json:"humidity"`
+	Cloudcover   int      `json:"cloudcover"`
+	FeelsLike    int      `json:"feelslike"`
+	UVIndex      int      `json:"uv_index"`
+	Visibiltiy   int      `json:"visibility"`
+	IsDay        string   `json:"is_day"`
 }
 
 var ApiKey string
@@ -65,23 +65,23 @@ var fetchCmd = &cobra.Command{
 		resp, err := http.Get("http://api.weatherstack.com/current?access_key=" + ApiKey + "&query=" + Location)
 
 		if err != nil {
-            panic(err)
+			panic(err)
 		}
 
-        defer resp.Body.Close()
+		defer resp.Body.Close()
 
-        var r Response
-        err = json.NewDecoder(resp.Body).Decode(&r)
-        if err != nil {
-            panic(err)
-        }
-        fmt.Println(r.Currents)
-        //var j interface{}
-        //err = json.NewDecoder(resp.Body).Decode(&j)
-        //if err != nil {
-          //  panic(err)
-        //}
-        //fmt.Println(j)
+		var r Response
+		err = json.NewDecoder(resp.Body).Decode(&r)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(r.Currents)
+		//var j interface{}
+		//err = json.NewDecoder(resp.Body).Decode(&j)
+		//if err != nil {
+		//  panic(err)
+		//}
+		//fmt.Println(j)
 	},
 }
 
@@ -93,11 +93,10 @@ func init() {
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// fetchCmd.PersistentFlags().String("foo", "", "A help for foo")
-    fetchCmd.PersistentFlags().StringVarP(&ApiKey, "apikey", "a", "", "Provide API key to fetch data from weatherstack")
-    fetchCmd.PersistentFlags().StringVarP(&Location, "location", "l", "", "Provide location")
+	fetchCmd.PersistentFlags().StringVarP(&ApiKey, "apikey", "a", "", "Provide API key to fetch data from weatherstack")
+	fetchCmd.PersistentFlags().StringVarP(&Location, "location", "l", "", "Provide location")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// fetchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
--- 14 - Cobra CLI/weather_app/cmd/root.go
+++ 14 - Cobra CLI/weather_app/cmd/root.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2022 fastbyt3
 */
 package cmd
 
@@ -10,13 +9,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "weather_app",
 	Short: "Weather app to get data from API",
-	Long: `Use cobra-cli to build a weather cli app`,
+	Long:  `Use cobra-cli to build a weather cli app`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -42,5 +39,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
